7dcf2f0a0de7791e8ae34538887afb9d7d164d65
Edward Z. Yang <ezyang@mit.edu>
2010-10-17 00:28:05 +0100

    Massively expand command set.
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>

--- mmr-hammer.hs
+++ mmr-hammer.hs
@@ -7,6 +7,7 @@ import Data.Char
 import Data.Maybe
 import Data.List
 import Data.IORef
+import Data.Time
 
 import Control.Monad
 import Control.Exception
@@ -19,15 +20,18 @@ import System.Environment
 import System.Console.GetOpt
 import System.Directory
 import System.FilePath
+import System.Locale
 
 import Text.Printf
+
+import Network.URI
 
 import LDAP.Init (ldapInitialize, ldapSimpleBind)
 import LDAP.Constants (ldapPort)
 import LDAP.Search (SearchAttributes(LDAPAllUserAttrs), LDAPEntry(..),
     LDAPScope(..), ldapSearch)
 import LDAP.Modify (LDAPModOp(..), LDAPMod(..), ldapAdd, ldapDelete,
-    ldapModify, list2ldm)
+    ldapModify, list2ldm, pairs2ldm)
 
 scriptsBase = "dc=scripts,dc=mit,dc=edu"
 configBase  = "cn=config"
@@ -36,10 +40,11 @@ replicaBase = "cn=replica,cn=\"dc=scripts,dc=mit,dc=edu\",cn=mapping tree,cn=con
 searchScripts = search scriptsBase
 searchConfig  = search configBase
 searchReplica = search replicaBase
-getEntry ldap dn = debugIOVal ("getEntry: " ++ dn) $
-    listToMaybe `fmap` ldapSearch ldap (Just dn) LdapScopeBase Nothing LDAPAllUserAttrs False
 search base ldap query = debugIOVal ("search: " ++ query ++ " -b " ++ base) $
     ldapSearch ldap (Just base) LdapScopeSubtree (Just query) LDAPAllUserAttrs False
+
+getEntry ldap dn = debugIOVal ("getEntry: " ++ dn) $
+    listToMaybe `fmap` ldapSearch ldap (Just dn) LdapScopeBase Nothing LDAPAllUserAttrs False
 
 normalizeKey    = map toLower
 lookupKey (normalizeKey -> key) attrs = maybe [] id $ lookup key (map (\(normalizeKey -> k,v)->(k,v)) attrs)
@@ -77,6 +82,7 @@ getBinds ldap = do
     case lookupKey "nsDS5ReplicaBindDN" attrs of
         [] -> error "getBinds: No binds found"
         bs -> return bs
+getConflicts ldap = searchScripts ldap "nsds5ReplConflict=*"
 
 -- what goes in when you create a replication agreement
 replicaConfig = constructKeySet
@@ -108,6 +114,7 @@ replicaRuntime = constructKeySet
     , "nsds5replicaLastInitStatus"
     , "nsds50ruv"
     , "nsruvReplicaLastModified"
+    , "nsds5beginreplicarefresh"
     ]
 
 ldapAddEntry ldap (LDAPEntry dn attrs) =
@@ -123,7 +130,7 @@ printAgreements ldap = do
 suspendAgreements ldap statefile = do
     replicas <- getAgreements ldap
     when (null replicas) $
-        error "suspendAgreements: Refusing to write empty replicas file"
+        error "suspendAgreements: Cowardly refusing to write empty replicas file"
     withFile statefile WriteMode $ \h ->
         hPutStr h (serializeEntries replicas)
     withFile (statefile ++ ".ldif") WriteMode $ \h ->
@@ -133,6 +140,17 @@ suspendAgreements ldap statefile = do
 restoreAgreements ldap statefile = do
     replicas <- fmap unserializeEntries (readFile statefile)
     mapM_ (ldapAddEntry ldap) replicas
+
+reinitAgreements ldap statefile = do
+    putStrLn "Disabling replication"
+    disableReplication ldap
+    putStrLn "Suspending agreements"
+    suspendAgreements ldap statefile
+    putStrLn "Restoring agreements"
+    restoreAgreements ldap statefile
+    putStrLn "Enabling replication"
+    enableReplication ldap
+    putStrLn "Done!"
 
 serializeEntries = show . map (\(LDAPEntry dn attrs) -> (dn, attrs))
 unserializeEntries = map (\(dn, attrs) -> LDAPEntry dn attrs) . read
@@ -148,31 +166,61 @@ printBinds ldap = do
 suspendBinds ldap statefile = do
     binds <- getBinds ldap
     when (null binds) $
-        error "suspendBinds: Refusing to write empty binds file"
+        error "suspendBinds: Cowardly refusing to write empty binds file"
     withFile statefile WriteMode $ \h ->
         hPutStr h (show binds)
     ldapModify ldap replicaBase [LDAPMod LdapModDelete "nsDS5ReplicaBindDN" []]
 
 restoreBinds ldap statefile = do
     binds <- fmap read (readFile statefile)
+    setBinds ldap binds
+
+setBinds ldap binds = do
+    oldBinds <- getBinds ldap
+    when (null oldBinds) $
+        error "restoreBinds: Cowardly refusing to overwrite non-empty binds on server"
     ldapModify ldap replicaBase [LDAPMod LdapModAdd "nsDS5ReplicaBindDN" binds]
 
 createLdap uri user password = do
+    debugIO $ "createLdap: Connecting to " ++ uri
     ldap <- ldapInitialize uri
     -- XXX LDAP has no support for other bind methods (yet)
     ldapSimpleBind ldap user password
     return ldap
 
-printStatus ldap = do
+forEachRawAgreement ldap f = do
     rawAgreements <- getRawAgreements ldap
     let width = maximum (0:concatMap (map length . lookupKey "nsDS5ReplicaHost" . leattrs) rawAgreements)
-    forM_ rawAgreements $ \(LDAPEntry dn attrs) -> do
-        let mhost   = lookupKey1 "nsDS5ReplicaHost" attrs
-            mstatus = lookupKey1 "nsds5replicaLastUpdateStatus" attrs
-        case (mhost, mstatus) of
-            (Just host, Just status) ->
-                printf ("%-" ++ show width ++ "s : %s\n") host status
-            otherwise -> warnIO ("Malformed replication agreement at " ++ dn)
+    forM_ rawAgreements (f width)
+
+printStatus ldap = forEachRawAgreement ldap f >> printConflicts ldap
+    where f width (LDAPEntry dn attrs) = do
+            let mhost   = lookupKey1 "nsDS5ReplicaHost" attrs
+                mstatus = lookupKey1 "nsds5replicaLastUpdateStatus" attrs
+                minitstatus = lookupKey1 "nsds5replicaLastInitStatus" attrs
+            case mhost of
+                (Just host) -> do
+                    let status = maybe "no status found" id mstatus
+                    printf ("%-" ++ show width ++ "s : %s\n") host status
+                otherwise -> warnIO ("Malformed replication agreement at " ++ dn)
+            case minitstatus of
+                (Just initstatus) -> putStrLn $ take width (repeat ' ') ++ " > " ++ initstatus
+                otherwise -> return ()
+
+printRUV ldap = forEachRawAgreement ldap f
+    where f width (LDAPEntry dn attrs) = do
+            let mhost = lookupKey1 "nsDS5ReplicaHost" attrs
+                ruvs = lookupKey "nsDS50ruv" attrs
+            putStrLn (maybe dn id mhost)
+            mapM_ (putStrLn . ("  " ++)) ruvs
+            return ()
+cleanRUV ldap = do
+    error "Not implemented yet"
+
+printConflicts ldap = do
+    conflicts <- getConflicts ldap
+    forM_ conflicts $ \(LDAPEntry dn attrs) ->
+        putStrLn dn
 
 getTarget ldap target = do
     dnMatch <- getEntry ldap target
@@ -198,6 +246,8 @@ update ldap target = do
                  "        see https://bugzilla.redhat.com/show_bug.cgi?id=637852"
         _ -> return ()
     ldapModify ldap dn [LDAPMod LdapModAdd "nsDS5BeginReplicaRefresh" ["start"]]
+    threadDelay 1000000
+    printStatus ldap
     --updateMonitor
 
 updateMonitor ldap target = do
@@ -207,6 +257,60 @@ updateMonitor ldap target = do
     -- XXX Find the status and report it, or exit
     updateMonitor ldap target
 printVersion ldap = getVersion ldap >>= putStrLn
+
+testDn = "scriptsVhostName=replication-test,ou=VirtualHosts," ++ scriptsBase
+
+testReplication ldap = do
+    resetTestReplication ldap
+    time <- formatTime defaultTimeLocale "%a-%b-%e-%H%M%S" `fmap` getCurrentTime
+    ldapAdd ldap testDn $ list2ldm LdapModAdd
+        [ ("objectClass",           ["top", "scriptsVhost"])
+        , ("scriptsVhostName",      ["replication-test"])
+        , ("scriptsVhostAlias",     ["replication-test-" ++ time])
+        , ("scriptsVhostAccount",   ["uid=signup,ou=People,dc=scripts,dc=mit,dc=edu"])
+        , ("scriptsVhostDirectory", [""])
+        ]
+
+setPluginEnabled ldap name status =
+    ldapModify ldap ("cn=" ++ name ++ ",cn=plugins,cn=config")
+        [LDAPMod LdapModReplace "nsslapd-pluginEnabled" [if status then "on" else "off"]]
+
+disableReplication ldap = do
+    setPluginEnabled ldap "Legacy Replication Plugin" False
+    setPluginEnabled ldap "Multimaster Replication Plugin" False
+enableReplication ldap = do
+    setPluginEnabled ldap "Multimaster Replication Plugin" True
+    setPluginEnabled ldap "Legacy Replication Plugin" True
+
+setSyntaxCheck ldap status =
+    ldapModify ldap ("cn=config")
+        [LDAPMod LdapModReplace "nsslapd-syntaxcheck" [if status then "on" else "off"]]
+
+disableSyntaxCheck ldap = setSyntaxCheck ldap False
+enableSyntaxCheck ldap = setSyntaxCheck ldap True
+
+resetTestReplication ldap = do
+    old <- getEntry ldap testDn
+    when (isJust old) $ ldapDelete ldap testDn
+    conflicts <- getConflicts ldap
+    forM_ conflicts $ \(LDAPEntry dn attrs) -> do
+        let orig = tail (dropWhile (/= '+') dn)
+        when (normalizeKey orig == normalizeKey testDn) $
+            ldapDelete ldap dn
+
+recoverUser ldap uid = do
+    ldapAdd ldap "dc=scripts,dc=mit,dc=edu" (list2ldm LdapModAdd
+        [ ("objectClass", ["top", "domain"])
+        , ("dc", ["scripts"])
+        ]) `catch` (\(e :: SomeException) -> print e)
+    ldapAdd ldap "ou=People,dc=scripts,dc=mit,dc=edu" (list2ldm LdapModAdd
+        [ ("objectClass", ["top", "organizationalunit"])
+        , ("ou", ["People"])
+        ]) `catch` (\(e :: SomeException) -> print e)
+    ldapAdd ldap ("uid=" ++ uid ++ ",ou=People,dc=scripts,dc=mit,dc=edu") (list2ldm LdapModAdd
+        [ ("objectClass", ["top", "account"])
+        , ("uid", [uid])
+        ]) `catch` (\(e :: SomeException) -> print e)
 
 data Password = Password String | AskPassword | NoPassword
 
@@ -250,7 +354,14 @@ parseOptions = do
         (_,_,errs) -> do
             hPutStr stderr (concat errs ++ usageInfo header options)
             exitFailure
-    where header = "Usage: mmr-hammer [print|suspend|restore] [binds|agreements]\n"
+
+    where header = "Usage: mmr-hammer [status|test|...] ...\n" ++
+                   "\n" ++
+                   "mmr-hammer is a command line tool for managing multimaster replication\n" ++
+                   "on Fedora 389 DS.\n" ++
+                   "\n" ++
+                   "Advanced commands: suspend, restore, set, reinit, disable, enable,\n" ++
+                   "                   agreements, binds, ruv, version, update, reset, recover"
 
 fillWith opts mUri mUser mPassword= do
     uri <- case optUri opts of
@@ -316,22 +427,49 @@ debugIOVal msg m = do
     m
 warnIO msg = hPutStrLn stderr $ "WARNING: " ++ msg
 
+usage v = putStrLn ("Usage: mmr-hammer " ++ v)
+
 main = do
-    let replicasFile = "mmr-hammer-replica"
-        bindsFile    = "mmr-hammer-binds"
     (opts, args) <- parseOptions
     when (optDebug opts) (writeIORef isDebugging True)
     (uri, user, password) <- tryAll $ [defaultStrategy, ldapVircStrategy, fallbackStrategy] `ap` [opts]
+    uristruct <- case parseURI uri of
+        Nothing -> error "Malformed URI"
+        Just uristruct -> return uristruct
+    let host = maybe "none" uriRegName (uriAuthority uristruct)
+        replicasFile = "mmr-hammer-replica-" ++ host
+        bindsFile    = "mmr-hammer-binds-" ++ host
     ldap <- createLdap uri user password
     case args of
-        ["print",   "binds"] -> printBinds ldap
-        ["suspend", "binds"] -> suspendBinds ldap bindsFile
-        ["restore", "binds"] -> restoreBinds ldap bindsFile
-        ["print",   "agreements"] -> printAgreements ldap
+        ["binds"] -> printBinds ldap
+        ["agreements"] -> printAgreements ldap
         ["suspend", "agreements"] -> suspendAgreements ldap replicasFile
+        ["suspend", "binds"] -> suspendBinds ldap bindsFile
         ["restore", "agreements"] -> restoreAgreements ldap replicasFile
+        ["restore", "binds"] -> restoreBinds ldap bindsFile
+        ["set", "binds"] -> usage "set binds uid=ldap/example.com,ou=People,dn=example,dn=com ..."
+        ("set": "binds": binds) -> setBinds ldap binds
+        ["reinit", "agreements"] -> reinitAgreements ldap replicasFile
+        ["disable", "replication"] -> disableReplication ldap
+        ["disable", "syntaxcheck"] -> disableSyntaxCheck ldap
+        ["enable", "replication"] -> enableReplication ldap
+        ["enable", "syntaxcheck"] -> enableSyntaxCheck ldap
         ["status"]  -> printStatus ldap
         ["version"] -> printVersion ldap
         ["update", target] -> update ldap target
-        [] -> putStrLn "mmr-hammer.hs [print|suspend|restore] [binds|agreements]"
-        _ -> error "Unknown argument"
+        ["test"] -> testReplication ldap
+        ["reset", "test"] -> resetTestReplication ldap
+        ["ruv"] -> printRUV ldap
+        ["recover", "user", uid] -> recoverUser ldap uid
+        ["cleanruv", target, replicaid] -> cleanRUV ldap target replicaid
+        ["conflicts"] -> printConflicts ldap
+        ("suspend": _) -> usage "suspend [agreements|binds]"
+        ("set":     _) -> usage "set [binds] VALUES..."
+        ("restore": _) -> usage "restore [agreements|binds]"
+        ("reinit":  _) -> usage "reinit [agreements]"
+        ("disable": _) -> usage "disable [replication|syntaxcheck]"
+        ("enable":  _) -> usage "enable [replication|syntaxcheck]"
+        ("reset":   _) -> usage "reset [test]"
+        ("recover": _) -> usage "recover [user]"
+        ("cleanruv":_) -> usage "cleanruv TARGET REPLICAID"
+        _ -> error "Unknown command"
